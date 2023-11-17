4bd9130625d56aba1e574b13a6a29424dbfa2433
Josee-cmd <josesslcedo@gmail.com>
2023-11-17 10:35:49 -0500

    Se agrego el registrar Tratamiento

---
+++ Controller/JS/sign up tratameinto.js
@@ -0,0 +1,15 @@
+let input = document.querySelector('#input-date'),
+    input2 = document.querySelector('#input2-date'),
+    lbl = document.querySelector('.lbl-date'),
+    lbl2 = document.querySelector('.lbl-fecha');
+
+input2.addEventListener('change', () => {
+    lbl.style.transform = 'translateY(-150%)';
+    input2.style.transform = 'translateX(-70%)';
+
+});
+
+input.addEventListener('change',()=>{
+    lbl2.style.transform = 'translateY(-150%)';
+    input.style.transform = 'translateX(-70%)';
+})
